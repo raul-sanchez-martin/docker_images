636f09a6ce6d6863f0a9ba9155f1aca4273de83f
Raul Sanchez Martin <r.sanchezma.2016@alumnos.urjc.es>
2017-12-12 23:28:24 +0100

    changes spark version to 2.0.2 and scala version to 2.11.8

--- spark_deep_learning/start_notebook.sh
+++ spark_deep_learning/start_notebook.sh
@@ -1,4 +1,4 @@
-BigDL_HOME="/environments/general_env/lib/python2.7/site-packages/bigdl"
+BigDL_HOME="/environments/general_env/lib/python3.5/site-packages/bigdl"
 BIGDL_VERSION="0.3.0"
 #SPARK_HOME="/usr/local/share/spark"
 MASTER="local[*]"
@@ -7,6 +7,9 @@ BigDL_JAR_PATH=${BigDL_HOME}/share/lib/bigdl-${BIGDL_VERSION}-jar-with-dependenc
 
 export PYTHONPATH=${PYTHON_API_ZIP_PATH}:$PYTHONPATH
 export PYSPARK_DRIVER_PYTHON="/environments/general_env/bin/jupyter"
+
+export PYSPARK_PYTHON="/environments/general_env/bin/python"
+
 export PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir=./  --ip=0.0.0.0 --port=8080 --no-browser --allow-root"
    
 ${SPARK_HOME}/bin/pyspark \
